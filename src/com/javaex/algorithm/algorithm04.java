85f31d3dc1875b54945fd9d6461d4c0de83d2610
한상선 <hs94122@naver.com>
2021-11-22 19:08:30 +0900

    2021-11-22 19:08

--- src/com/javaex/algorithm/algorithm04.java
+++ src/com/javaex/algorithm/algorithm04.java
@@ -3,7 +3,11 @@ package com.javaex.algorithm;
 public class algorithm04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
 
 	}
 
